ae7c95dbe2457c85d332b295df4bb5f4a96a5aba
yojana-at-426337347448 <kushalj001@gmail.com>
2019-01-24 20:23:13 +0530

    added comments

--- Gender Prediction/gender_script.py
+++ Gender Prediction/gender_script.py
@@ -18,7 +18,8 @@ for ch in text:
 
 words = clean.split()
 x = 0
-#print(names)
+
+# Words related to male and female gender respectively. Adding more words to this list will result in better accuracy
 male_words = ['him','himself','he','man','brother','father','widower','half-brother','actor','host','Sir','king','husband','son','Earl','Duke','Emperor','emperor','Prince','monk','businessman','boy','uncle']
 female_words = ['her','herself','she','woman','lady','women','actress','sister','mother','half-sister','actress','Queen','queen','wife','daughter','widow','Duchess','Princess','nun','girl','aunt']
 
@@ -27,19 +28,22 @@ for i,name in enumerate(names):
     indices = []
     all_appearences = []
     indices = [i for i,x in enumerate(words) if x==name]
-    #print(indices)
+
+    # Fetch all appearences of the name from the corpus
     for index in indices:
         start = index - 2
         end = index + 10
         window = words[start:end]
         all_appearences.append(window)
 
+    # Check for salutation/title,since this gives a more a clear indication of the gender
     for appearance in all_appearences:
         if appearance[1] in ['Mr.','Sir','King']:
             name_gender[name] += 100
         elif appearance[1] in ['Mrs.','Miss','Queen']:
             name_gender[name] -= 100
 
+    # Other determinants of the gender        
         for word in appearance:
             if word in male_words:
                 name_gender[name] += 5
--- Matching Book Names/match.py
+++ Matching Book Names/match.py
@@ -1,3 +1,4 @@
+import imp
 import numpy as np
 import pandas as pd
 import re
@@ -10,31 +11,48 @@ book_names = [str(input()) for i in range(n)]
 delimiter =  input()
 descriptions = [str(input()) for i in range(n)]
 
-#desc_sentences = nltk.sent_tokenize(descriptions)
 descriptions_df = pd.DataFrame(descriptions,columns=['desc'])
 
-stopwords = nltk.corpus.stopwords.words('english')
+
+# Downloading the nltk stopwords takes time and results in timeout.
+stopwords = ['i', 'me', 'my', 'myself', 'we', 'our', 'ours', 'ourselves', 'you', "you're", "you've", "you'll", "you'd", 'your', 'yours',
+  'yourself', 'yourselves', 'he', 'him', 'his', 'himself', 'she', "she's", 'her', 'hers', 'herself', 'it', "it's", 'its', 'itself', 'they',
+  'them','their','theirs','themselves','what','which','who','whom','this','that',"that'll",'these',
+  'those', 'am', 'is', 'are', 'was', 'were', 'be', 'been', 'being', 'have', 'has', 'had', 'having', 'do',
+  'does', 'did', 'doing' ,'a', 'an', 'the', 'and', 'but', 'if', 'or', 'because', 'as', 'until', 'while',
+  'of', 'at', 'by', 'for', 'with', 'about', 'against', 'between', 'into', 'through', 'during', 'before', 'after',
+  'above', 'below', 'to', 'from', 'up', 'down', 'in', 'out', 'on', 'off', 'over', 'under' ,'again', 'further',
+  'then', 'once', 'here', 'there', 'when', 'where', 'why', 'how', 'all', 'any', 'both', 'each', 'few', 'more',
+  'most', 'other', 'some', 'such', 'no', 'nor', 'not', 'only', 'own', 'same', 'so', 'than', 'too', 'very', 's', 't',
+  'can', 'will', 'just', 'don', "don't", 'should', "should've", 'now', 'd', 'll', 'm', 'o', 're', 've', 'y', 'ain', 'aren',
+  'couldn', "couldn't", 'didn', "didn't", 'doesn', "doesn't", 'hadn', "hadn't", 'hasn', "hasn't", 'haven', "haven't", 'isn',
+  "isn't", 'ma', 'mightn', "mightn't", 'mustn', "mustn't", 'needn', "needn't", 'shan', "shan't", 'shouldn', "shouldn't",
+  'wasn', "wasn't", 'weren', "weren't", 'won', "won't", 'wouldn', "wouldn't"]
 
 def preprocess(text):
     '''Combination of all the necassary text preprocessing'''
-    text = ''.join([char for char in text if char not in string.punctuation])
     tokens = re.split('\W+',text)
     tokens = [word.lower() for word in tokens]
-    text = " ".join([word for word in tokens if word not in stopwords])
 
     return text
 
+# Fitting a TF-IDF vectorizer on description docs
 descriptions_df['cleaned'] = descriptions_df['desc'].apply(lambda x:preprocess(x))
-vectorizer = TfidfVectorizer()
+vectorizer = TfidfVectorizer(stop_words = stopwords)
 features = vectorizer.fit_transform(descriptions_df['cleaned'])
 
-#book_names = book_names.split('\n')
+# Transforming book names into feature vectors according to the vocabulary learnt based on dictionaries
 book_names_df = pd.DataFrame(book_names,columns=['book names'])
 name_features = vectorizer.transform(book_names_df['book names'])
 
+# Converting features into arrays
 desc_vectors = features.toarray()
 book_vectors = name_features.toarray()
+
+# Dot product to calculate the closest matching book for each description
 x = np.dot(book_vectors,desc_vectors.T)
 answers = list(np.argmax(x,axis=0))
+
+# Print answers
 for answer in answers:
     print(answer+1)
